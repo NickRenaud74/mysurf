8a9c82ae7cf4df6396f07605692c32f942daa270
NickRenaud74 <nick_renaud@hotmail.com>
2021-08-17 11:40:08 -0700

    establish env variables and setup DB connection

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+//Mongoose and database setup
+const mongoose = require('mongoose')
+const dbPassword = process.env.DB_PASS;
+const mongoDb = `mongodb+srv://mySurf:${dbPassword}@mysurfcluster.y2bdm.mongodb.net/mySurf?retryWrites=true&w=majority`;
+mongoose.connect(mongoDb, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error: '))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
