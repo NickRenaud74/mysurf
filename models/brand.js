cbbd94c932d4a76fdf49813bf21271f1d46e4d46
NickRenaud74 <nick_renaud@hotmail.com>
2021-08-18 10:19:33 -0700

    Create  models for DB

---
+++ models/brand.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const BrandSchema = new Schema({
+    title: {type: String, required: true, minLength: 2, maxLength: 30},
+    description: {type: String, required: true, minLength: 3, maxLength: 100},
+    image: {type: String}
+});
+
+BrandSchema
+.virtual('url')
+.get(function() {
+    return '/brand/' + this._id;
+})
+
+module.exports = mongoose.model('Brand', BrandSchema);
---
+++ models/product.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+    name: {type: String, required: true, maxLength: 25},
+    description: {type: String, required: true, minLength: 3, maxLength: 500},
+    category: {type: Schema.Types.ObjectId, ref: 'Category', required: true},
+    price: {type: Number, required: true, min: 1, max: 999999 },
+    inStock: {type: Number, required: true, min: 0, max: 99},
+    brand: {type: Schema.Types.ObjectId, ref: 'Brand', required: true},
+    image: {type: String}
+})
+
+ProductSchema
+.virtual('url')
+.get( function() {
+    return '/product/' + this._id;
+})
+
+module.exports = mongoose.model('Product', ProductSchema)
