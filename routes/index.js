c3c760bf7b4dd65b2427cd5c87c1cafbbcf1c766
NickRenaud74 <nick_renaud@hotmail.com>
2021-09-04 09:07:56 -0700

    refactor routes, delete unused routes, add multer for image upload

--- routes/index.js
+++ routes/index.js
@@ -22,55 +22,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'My Surf' });
 });
 
+
 //BRAND ROUTES //
-//Get request for creating a brand.
-router.get('/brand/create', brandController.brandCreateGet);
-
-//Post request for creating a brand
-router.post('/brand/create', brandController.brandCreatePost);
-
-//Get request for deleting a brand
-router.get('/brand/:id/delete', brandController.brandDeleteGet);
-
-//Post request for deleting a brand
-router.post('/brand/:id/delete', brandController.brandDeletePost);
-
-//Get request for updating a brand
-router.get('/brand/:id/update', brandController.brandUpdateGet);
-
-//Post request for updating a brand
-router.get('/brand/:id/update', brandController.brandUpdatePost);
-
-//Get request for a single brand
+//GET for a single brand
 router.get('/brand/:id', brandController.brandDetail);
 
-//Get request for all brands
+//GET for all brands
 router.get('/brands', brandController.brandList);
 
+
 //CATEGORY ROUTES //
-
-//Get request for creating a category.
-router.get('/category/create', categoryController.categoryCreateGet);
-
-//Post request for creating a category
-router.post('/category/create', categoryController.categoryCreatePost);
-
-//Get request for deleting a category
-router.get('/category/:id/delete', categoryController.categoryDeleteGet);
-
-//Post request for deleting a category
-router.post('/category/:id/delete', categoryController.categoryDeletePost);
-
-//Get request for updating a category
-router.get('/category/:id/update', categoryController.categoryUpdateGet);
-
-//Post request for updating a category
-router.get('/category/:id/update', categoryController.categoryUpdatePost);
-
-//Get request for a single category
+//GET for a single category
 router.get('/category/:id', categoryController.categoryDetail);
 
-//Get request for all categorys
+//GET for all categories
 router.get('/categories', categoryController.categoryList);
 
 
@@ -82,17 +47,11 @@ router.get('/product/create', productController.productCreateGet);
 //Post request for creating a product
 router.post('/product/create', upload.single('image'), productController.productCreatePost);
 
-//Get request for deleting a product
-router.get('/product/:id/delete', productController.productDeleteGet);
-
 //Post request for deleting a product
 router.post('/product/:id/delete', productController.productDeletePost);
 
-//Get request for updating a product
-router.get('/product/:id/update', productController.productUpdateGet);
-
 //Post request for updating a product
-router.get('/product/:id/update', productController.productUpdatePost);
+router.post('/product/:id/update', upload.single('image'), productController.productUpdatePost);
 
 //Get request for a single product
 router.get('/product/:id', productController.productDetail);
